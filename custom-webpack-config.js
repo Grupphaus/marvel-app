0d4b0304f38c883d622be9dcc0f68ab32ee46670
Federico Pellegrini <fellegrini@gmail.com>
2022-05-12 19:57:39 -0300

    build goes errrr

--- custom-webpack-config.js
+++ custom-webpack-config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = (config) => {
   config.plugins.push(
     new webpack.DefinePlugin({
-      "process.env['API_KEY']": JSON.stringify(process.env.API_KEY),
+      "process.env.API_KEY": JSON.stringify(process.env.API_KEY),
     })
   );
 
