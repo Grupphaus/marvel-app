f353875534feebcfa199f1b9b7de95710e7cc9ca
Federico Pellegrini <fellegrini@gmail.com>
2022-05-12 08:38:28 -0300

    last try?

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+        apiUrl: 'https://gateway.marvel.com:443/v1/public',
+        key: process.env.API_KEY,
+        appVersion: '0.0.0',
+        production: true,
+    };
+  
