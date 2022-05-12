555d3fe605ecf601ab8c03debfab1f5accac8bdd
Federico Pellegrini <fellegrini@gmail.com>
2022-05-12 19:33:42 -0300

    test

--- custom-webpack-config.js
+++ custom-webpack-config.js
@@ -1,9 +1,11 @@
 const webpack = require('webpack');
 
-module.exports = {
-  plugins: [
+module.exports = (config) => {
+  config.plugins.push(
     new webpack.DefinePlugin({
       "process.env['API_KEY']": JSON.stringify(process.env.API_KEY),
     })
-  ]
+  );
+
+  return config;
 };
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,5 +51,3 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
-(window as any).process = { env: { DEBUG: undefined }, };
