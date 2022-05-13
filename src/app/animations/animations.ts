faafb469f7b702cbe349278bbf96a73c5425589e
Federico Pellegrini <fellegrini@gmail.com>
2022-05-13 04:35:18 -0300

    not found component mobile adjustments

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -3,19 +3,16 @@ import { trigger, animate, transition, style, query } from '@angular/animations'
 export const fadeAnimation = trigger('fadeAnimation', [
   transition('* => *', [
     query(
-      ':enter',
-      [style({ opacity: 0 })],
-      { optional: true }
-    ),
+      ':enter', [
+        style({ opacity: 0 })],
+      { optional: true }),
     query(
-      ':leave',
-      [style({ opacity: 1 }), animate('0.2s', style({ opacity: 0 }))],
-      { optional: true }
-    ),
+      ':leave', [
+        style({ opacity: 1 }), animate('0.2s', style({ opacity: 0 }))],
+      { optional: true }),
     query(
-      ':enter',
-      [style({ opacity: 0 }), animate('0.2s', style({ opacity: 1 }))],
-      { optional: true }
-    )
+      ':enter', [
+        style({ opacity: 0 }), animate('0.2s', style({ opacity: 1 }))],
+      { optional: true }),
   ])
 ]);
